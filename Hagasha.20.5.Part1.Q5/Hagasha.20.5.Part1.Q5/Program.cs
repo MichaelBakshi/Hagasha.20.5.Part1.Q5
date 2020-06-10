527e844f2690e5e776469af842b0766d36a6db01
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-06-10 18:12:25 +0300

    Add files via upload

---
+++ Hagasha.20.5.Part1.Q5/Hagasha.20.5.Part1.Q5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagasha._20._5.Part1.Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int total3 = TotalMarks(8, 9, 8);
+            Console.WriteLine(total3);
+        }
+
+            // We will use params when we need to run a function for an array 
+            // with undefined length (number of values)
+
+            public static int TotalMarks(params int[] list)
+            {
+                int total = 0;
+                for (int i = 0; i < list.Length; i++)
+                    total += list[i];
+                return total;
+            }
+
+        
+    }
+}
